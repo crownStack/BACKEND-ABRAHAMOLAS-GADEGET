a859e8f872c3457705c269d46210eaa9fd6c8371
Fatade Adebola Victor <158852298+fatadde@users.noreply.github.com>
2026-09-22 02:12:41 +0100

    publish

--- db/config.js
+++ db/config.js
@@ -2,8 +2,4 @@ const dns = require('dns');
 const mongoose = require('mongoose');
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-if (!process.env.MONGODB_URI) {
-	throw new Error('MONGODB_URI is not configured');
-}
-
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb+srv://fatadeadebola7_db_user:nqf5gDiQMcFuvUSt@cluster0.ezsxlz0.mongodb.net/")
